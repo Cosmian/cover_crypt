cf34b7f7d1dd722812e039172688034c5464983a
Hugo Rosenkranz-Costa <hugo.rosenkranz@cosmian.com>
2022-12-01 12:14:05 +0100

    chore(PyO3): create proper python package directory

--- python/scripts/test.sh
+++ python/scripts/test.sh
@@ -5,6 +5,6 @@ rm -f target/wheels/*.whl
 maturin build --release --features python
 pip install --force-reinstall target/wheels/*.whl
 # Test typing
-mypy python/test_cover_crypt.py
+mypy python/scripts/test_cover_crypt.py
 # Unit tests
-python3 python/test_cover_crypt.py
+python3 python/scripts/test_cover_crypt.py
