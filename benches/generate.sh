ab1d72b6c7aa320d7b114506e1ed2159a9529644
Manuthor <manu.coste@gmail.com>
2023-02-28 10:50:43 +0100

    docs(readme): update readme

--- benches/generate.sh
+++ benches/generate.sh
@@ -1,8 +1,11 @@
-# Usage: bash generate.sh
-
 #!/bin/sh
 
 set -e
+
+# Usage: bash generate.sh
+
+cargo install cargo-criterion
+cargo install criterion-table
 
 cargo criterion --features full_bench --message-format=json | criterion-table >benches/BENCHMARKS_classic.md
 cargo criterion --features full_bench,hybridized_bench --message-format=json | criterion-table >benches/BENCHMARKS_hybridized.md
