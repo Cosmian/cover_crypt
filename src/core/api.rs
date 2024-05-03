bc24d192e01b4d9faf782e9b80e1b31fcb54b64b
Théophile BRÉZOT <theophile.brezot@cosmian.com>
2024-05-03 17:16:51 +0200

    remove duplicated keygen

--- src/core/api.rs
+++ src/core/api.rs
@@ -104,7 +104,159 @@ fn main() {
         .unwrap()
         .is_none());
 }
+<<<<<<< HEAD
 =======
+=======
+
+impl Default for Covercrypt {
+    fn default() -> Self {
+        Self {
+            rng: Mutex::new(CsRng::from_entropy()),
+        }
+    }
+}
+
+impl PartialEq for Covercrypt {
+    fn eq(&self, _other: &Self) -> bool {
+        true
+    }
+}
+
+impl Covercrypt {
+    /// Sets up the Covercrypt scheme.
+    ///
+    /// Generates a MSK and a MPK with a tracing level of
+    /// [`MIN_TRACING_LEVEL`](core::MIN_TRACING_LEVEL).
+    /// They only hold keys for the origin coordinate: only broadcast
+    /// encapsulations can be created.
+    pub fn setup(&self) -> Result<(MasterSecretKey, MasterPublicKey), Error> {
+        let mut msk = setup(
+            &mut *self.rng.lock().expect("Mutex lock failed!"),
+            MIN_TRACING_LEVEL,
+        )?;
+
+        // Add broadcast coordinate with classic encryption level.
+        //
+        // TODO replace this function by `add_coordinates`,
+        // `remove_coordinates`, `hybridize_coordinates` and
+        // `deprecate_coordinates`.
+        update_coordinate_keys(
+            &mut *self.rng.lock().expect("Mutex lock failed!"),
+            &mut msk,
+            HashMap::from_iter([(
+                Coordinate::from_attribute_ids(vec![])?,
+                (EncryptionHint::Classic, AttributeStatus::EncryptDecrypt),
+            )]),
+        )?;
+        let mpk = mpk_keygen(&msk)?;
+
+        Ok((msk, mpk))
+    }
+
+    /// Updates the MSK according to this policy. Returns the new version of the
+    /// MPK.
+    ///
+    /// Sets the MPK coordinates to the one defined by the policy:
+    /// - removes coordinates from the MSK that don't belong to the new policy
+    /// along with their associated keys;
+    /// - adds the policy coordinates that don't belong yet to the MSK,
+    /// generating new keys.
+    ///
+    /// The new MPK holds the latest public keys of each coordinates of the new policy.
+    pub fn update_master_keys(
+        &self,
+        policy: &Policy,
+        msk: &mut MasterSecretKey,
+    ) -> Result<MasterPublicKey, Error> {
+        update_coordinate_keys(
+            &mut *self.rng.lock().expect("Mutex lock failed!"),
+            msk,
+            policy.generate_universal_coordinates()?,
+        )?;
+        let mpk = mpk_keygen(msk)?;
+        Ok(mpk)
+    }
+
+    /// Generates new keys for each coordinate in the semantic space of the
+    /// given access policy and update the given master keys.
+    ///
+    /// All user keys need to be refreshed.
+    // TODO document error cases.
+    pub fn rekey(
+        &self,
+        ap: &AccessPolicy,
+        policy: &Policy,
+        msk: &mut MasterSecretKey,
+    ) -> Result<MasterPublicKey, Error> {
+        rekey(
+            &mut *self.rng.lock().expect("Mutex lock failed!"),
+            msk,
+            policy.generate_semantic_space_coordinates(ap)?,
+        )?;
+        mpk_keygen(msk)
+    }
+
+    /// Removes all but the latest secret of each coordinate in the semantic
+    /// space of the given access policy from the given master keys.
+    ///
+    /// This action is *irreversible*, and all user keys need to be refreshed.
+    // TODO document error cases.
+    pub fn prune_master_secret_key(
+        &self,
+        access_policy: &AccessPolicy,
+        policy: &Policy,
+        msk: &mut MasterSecretKey,
+    ) -> Result<MasterPublicKey, Error> {
+        prune(
+            msk,
+            &policy.generate_semantic_space_coordinates(access_policy)?,
+        );
+        mpk_keygen(msk)
+    }
+
+    /// Generates a USK associated to the given access policy.
+    ///
+    /// It will be given the latest secret of each coordinate in the semantic
+    /// space of its access policy.
+    // TODO document error cases.
+    pub fn generate_user_secret_key(
+        &self,
+        msk: &mut MasterSecretKey,
+        access_policy: &AccessPolicy,
+        policy: &Policy,
+    ) -> Result<UserSecretKey, Error> {
+        usk_keygen(
+            &mut *self.rng.lock().expect("Mutex lock failed!"),
+            msk,
+            policy.generate_semantic_space_coordinates(access_policy)?,
+        )
+    }
+
+    /// Refreshes the USK relatively to the given MSK and policy.
+    ///
+    /// The USK will be given the latest secrets of each coordinate in the
+    /// semantic space of its access policy and secrets that have been removed
+    /// from the MSK will be removed. If `keep_old_rights` is set to false, only
+    /// the latest secret of each coordinate is kept instead.
+    ///
+    /// Updates the tracing level to match the one of the MSK if needed.
+    // TODO document error cases.
+    pub fn refresh_usk(
+        &self,
+        usk: &mut UserSecretKey,
+        msk: &mut MasterSecretKey,
+        keep_old_rights: bool,
+    ) -> Result<(), Error> {
+        refresh(
+            &mut *self.rng.lock().expect("Mutex lock failed!"),
+            msk,
+            usk,
+            keep_old_rights,
+        )
+    }
+}
+
+>>>>>>> 9cfcbf7 (remove duplicated keygen)
 /// Authenticated Encryption trait
 pub trait AE<const KEY_LENGTH: usize, const NONCE_LENGTH: usize, const MAC_LENGTH: usize> {
     const KEY_LENGTH: usize;
