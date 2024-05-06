9b783c71a8cd32799bce2017335bdc6a7b60eb10
Adam Khayam <adamkhayam@Adams-MacBook-Pro.local>
2024-05-06 14:10:00 +0200

    refactoring ae and encrypted_header

---
+++ src/core/ae.rs
@@ -0,0 +1,47 @@
+use cosmian_crypto_core::{
+    reexport::rand_core::CryptoRngCore, Aes256Gcm, Dem, FixedSizeCBytes, Instantiable, Nonce,
+    RandomFixedSizeCBytes, SymmetricKey,
+};
+
+use crate::Error;
+
+/// Authenticated Encryption trait
+pub trait AE<const KEY_LENGTH: usize> {
+    /// Encrypts the given plaintext `ptx` using the given `key`.
+    fn encrypt(
+        key: &SymmetricKey<KEY_LENGTH>,
+        ptx: &[u8],
+        rng: &mut impl CryptoRngCore,
+    ) -> Result<Vec<u8>, Error>;
+
+    /// Decrypts the given ciphertext `ctx` using the given `key`.
+    ///
+    /// # Error
+    ///
+    /// Returns an error if the integrity of the ciphertext could not be verified.
+    fn decrypt(key: &SymmetricKey<KEY_LENGTH>, ctx: &[u8]) -> Result<Vec<u8>, Error>;
+}
+
+impl AE<{ Self::KEY_LENGTH }> for Aes256Gcm {
+    fn encrypt(
+        key: &SymmetricKey<{ Self::KEY_LENGTH }>,
+        ptx: &[u8],
+        rng: &mut impl CryptoRngCore,
+    ) -> Result<Vec<u8>, Error> {
+        let nonce = Nonce::<{ Self::NONCE_LENGTH }>::new(&mut *rng);
+        let ciphertext = Self::new(key).encrypt(&nonce, ptx, None)?;
+        Ok([nonce.as_bytes(), &ciphertext].concat())
+    }
+
+    fn decrypt(key: &SymmetricKey<{ Self::KEY_LENGTH }>, ctx: &[u8]) -> Result<Vec<u8>, Error> {
+        if ctx.len() < Self::NONCE_LENGTH {
+            return Err(Error::CryptoCoreError(
+                cosmian_crypto_core::CryptoCoreError::DecryptionError,
+            ));
+        }
+        let nonce = Nonce::try_from_slice(&ctx[..Self::NONCE_LENGTH])?;
+        Self::new(key)
+            .decrypt(&nonce, &ctx[Self::NONCE_LENGTH..], None)
+            .map_err(Error::CryptoCoreError)
+    }
+}
