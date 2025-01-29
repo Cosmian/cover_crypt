730612f9f8fd0417b8d91ce49fd9e705cca73a6c
phochard <pauline.hochard@cosmian.com>
2025-01-29 15:46:57 +0100

    Revert "test to expose AccessStructure"
    This reverts commit 771a1438b9ae852bfc27d94f8265aa58bb65115e.

--- src/lib.rs
+++ src/lib.rs
@@ -19,8 +19,7 @@ mod encrypted_header;
 pub mod api;
 pub mod traits;
 
-pub use abe_policy::{EncryptionHint, QualifiedAttribute};
-pub use abe_policy::AccessStructure;
+pub use abe_policy::{EncryptionHint, QualifiedAttribute, AccessStructure};
 
 #[cfg(any(test, feature = "test-utils"))]
 pub mod test_utils;
