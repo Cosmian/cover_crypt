3cc33db63165ffd5873b97211326b891d60d25c6
Manuthor <manu.coste@gmail.com>
2025-02-19 13:55:14 +0100

    ci: use bash script instead of reusable workflow

--- .github/scripts/cargo.sh
+++ .github/scripts/cargo.sh
@@ -1,13 +1,16 @@
-cargo fmt --check || exit 1
+#!/bin/bash
 
-cargo clippy --no-deps --all-targets -- -D warnings \
-    || cargo clippy --no-deps --all-targets --no-default-features --features curve25519,mlkem-768 -- -D warnings \
-    || cargo clippy --no-deps --all-targets --no-default-features --features p-256,mlkem-512 -- -D warnings \
-    || cargo clippy --no-deps --all-targets --no-default-features --features p-256,mlkem-768 -- -D warnings \
-    || exit 1
+set -ex
 
-cargo test \
-    || cargo test --no-default-features --features curve25519,mlkem-768 \
-    || cargo test --no-default-features --features p-256,mlkem-512 \
-    || cargo test --no-default-features --features p-256,mlkem-768 \
-    || exit 1
+cargo fmt --check
+
+cargo clippy --no-deps --all-targets -- -D warnings
+
+cargo clippy --no-deps --all-targets --no-default-features --features curve25519,mlkem-768 -- -D warnings
+cargo clippy --no-deps --all-targets --no-default-features --features p-256,mlkem-512 -- -D warnings
+cargo clippy --no-deps --all-targets --no-default-features --features p-256,mlkem-768 -- -D warnings
+
+cargo test
+cargo test --no-default-features --features curve25519,mlkem-768
+cargo test --no-default-features --features p-256,mlkem-512
+cargo test --no-default-features --features p-256,mlkem-768
