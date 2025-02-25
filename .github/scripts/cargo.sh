a4b2b820a97196d1334221622d20305aa36cbf57
Manuthor <manu.coste@gmail.com>
2025-02-25 12:25:59 +0100

    fix: check CVE with cargo deny

--- .github/scripts/cargo.sh
+++ .github/scripts/cargo.sh
@@ -6,6 +6,8 @@ cargo fmt --check
 
 cargo clippy --no-deps --all-targets -- -D warnings
 
+cargo install --locked cargo-deny && cargo deny check
+
 cargo clippy --no-deps --all-targets --no-default-features --features curve25519,mlkem-768 -- -D warnings
 cargo clippy --no-deps --all-targets --no-default-features --features p-256,mlkem-512 -- -D warnings
 cargo clippy --no-deps --all-targets --no-default-features --features p-256,mlkem-768 -- -D warnings
